5d12177824cf67dddad2fac18e82bbf01c848f36
giancarlorhodes <giancarlo.rhodes@gmail.com>
2020-12-16 11:20:14 -0600

    Add project files.

---
+++ firstalgos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstalgos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
