dd103a23c47abd4e8827e21fc9587c13f4ff48ac
Giancarlo Rhodes <giancarlo.rhodes@onshoreoutsourcing.com>
2021-11-18 09:42:39 -0600

    arrays and lists

---
+++ ConvertAllArrays/Parse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertAllArrays
+{
+    class Parse {
+
+
+        public bool IsNumeric(string value)
+        {
+            return value.All(char.IsNumber);
+        }
+
+        public string[] ParseArray(object[] inArray) 
+        {
+            List<string> temp = new List<string>();
+
+            // add implementation here
+
+            // take each element
+
+
+            // if number, convert it and store it list
+
+            // if string, no conversion needed, just store
+
+            foreach (var item in inArray)
+            {
+
+             
+                if (this.IsNumeric(item.ToString()))
+                {
+                    temp.Add(item.ToString());
+                }
+                else {
+                    temp.Add(item.ToString());                
+                } 
+            }
+
+            return temp.ToArray();
+
+        }
+
+
+    }
+}
---
+++ ConvertAllArrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertAllArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // Create a function that takes an array of integers and strings, converts integers to strings, 
+            // and returns the array as a string array.
+
+            //Examples
+            //ParseArray([1, 2, "a", "b"]) ➞ ["1", "2", "a", "b"]
+
+            //ParseArray(["abc", 123, "def", 456]) ➞ ["abc", "123", "def", "456"]
+
+            //ParseArray([1, 2, 3, 17, 24, 3, "a", "123b"]) ➞ ["1", "2", "3", "17", "24", "3", "a", "123b"]
+
+            //ParseArray([]) ➞ []
+
+            Parse p = new Parse();
+            object[] o = new object[4];
+            o[0] = 1;
+            o[1] = 100;
+            o[2] = "aaa";
+            o[3] = "b";
+
+            string[] _return = p.ParseArray(o);
+
+
+            foreach (var item in _return)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+        }
+    }
+}
